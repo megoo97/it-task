c5562c1c7a6f9a4440a8a82b7310299aee9c3f2a
megoo97 <32550377+megoo97@users.noreply.github.com>
2018-03-06 12:45:27 -0800

    this first edition of the project

---
+++ laravelapp/database/migrations/2018_03_06_190941_create_carts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->integer('productId');
+            $table->integer('userId');
+            /*$table->foreign('productId')->references('id')->on('products');
+            $table->foreign('userId')->references('id')->on('users_datas');
+            $table->primary(array('productId'));*/
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
